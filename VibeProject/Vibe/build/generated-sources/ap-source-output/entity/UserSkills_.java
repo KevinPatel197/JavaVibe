814f9613586d832c533f6fc264535b14919d9787
Kevin Patel <kevinpatel6221@gmail.com>
2023-05-27 20:27:46 +0530

    ego

--- VibeProject/Vibe/build/generated-sources/ap-source-output/entity/UserSkills_.java
+++ VibeProject/Vibe/build/generated-sources/ap-source-output/entity/UserSkills_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-24T10:11:47")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-27T19:01:28")
 @StaticMetamodel(UserSkills.class)
 public class UserSkills_ { 
 
