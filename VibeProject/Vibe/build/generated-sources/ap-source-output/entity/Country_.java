c475447f7aed8846fcd28776b237ab4c5df34395
Kevin Patel <kevinpatel6221@gmail.com>
2023-06-18 19:41:43 +0530

    icons

--- VibeProject/Vibe/build/generated-sources/ap-source-output/entity/Country_.java
+++ VibeProject/Vibe/build/generated-sources/ap-source-output/entity/Country_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-17T09:29:54")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-17T19:41:11")
 @StaticMetamodel(Country.class)
 public class Country_ { 
 
--- VibeProject/Vibe/build/generated-sources/ap-source-output/entity/UserContactInfo_.java
+++ VibeProject/Vibe/build/generated-sources/ap-source-output/entity/UserContactInfo_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-17T09:29:54")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-17T19:41:11")
 @StaticMetamodel(UserContactInfo.class)
 public class UserContactInfo_ { 
 
